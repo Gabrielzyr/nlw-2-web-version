fe34ce7dbda9b6c375e96e3078f38fd758e50cda
GabrielZyr <gabrielmz397@gmail.com>
2020-08-03 21:31:05 -0300

    teachers list

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,6 +4,7 @@ import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 function Landing() {
@@ -22,15 +23,15 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="#" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="#" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassIcon} alt="Estudar"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
