3c90731c3a45b1d1a8978faacc887cd41b477061
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2020-08-06 20:49:13 -0300

    add Favorits and Teacher list structure

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -17,23 +17,32 @@ function Landing() {
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
   }
+
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={landingImg} style={styles.banner}/>
+      <Image source={landingImg} style={styles.banner} />
 
       <Text style={styles.title}>
         Seja bem-vindo, {'\n'}
         <Text style={styles.titleBold}>O que deseja fazer?</Text>
       </Text>
       <View style={styles.buttonsContainer}>
-      
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+
+        <RectButton
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
 
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
-        
-        <RectButton onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}>
           <Image source={giveClassesIcon} />
 
           <Text style={styles.buttonText}>Dar aulas</Text>
---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './styles';
+
+function TeacherList() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Lista de Proffys
+      </Text>
+    </View>
+  );
+}
+
+export default TeacherList;
