ad227ca2d9b00079e1b39e56fb52bcda97497a6b
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2020-08-07 21:35:10 -0300

    Teacher card

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,14 +1,63 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ScrollView, TextInput } from 'react-native'
 
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
+import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList() {
+ const [isFilterVisible, setIsFilterVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis"/>
+      <PageHeader title="Proffys disponíveis" >
+       { isFilterVisible &&(  
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Qual a matéria?"
+            placeholderTextColor="#c1bccc"
+          />
+          <View style={styles.inputGroup}>
+            
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o dia?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+            
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual horário?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+
+          </View>
+        </View>
+        )}
+      </PageHeader>
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 24,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
